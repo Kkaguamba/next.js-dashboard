43c08cd71c0a5bd07c9e2c311a9a4a6a73ff6fac
kkaguamba <kkaguamba@gmail.com>
2026-09-14 14:03:17 +0300

    updatingInvoice

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,17 +1,14 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
-
+ 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
-  const [invoice, customers] = await Promise.all([
+   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
-
- 
-export default async function Page() {
   return (
     <main>
       <Breadcrumbs
@@ -27,4 +24,4 @@ export default async function Page() {
       <Form invoice={invoice} customers={customers} />
     </main>
   );
-} 
+}
